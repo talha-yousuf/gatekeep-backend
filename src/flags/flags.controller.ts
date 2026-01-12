46f7b536a3dc770102126aa962758ac3b4984063
Talha Yousuf <talhayousuf.work@gmail.com>
2026-01-13 01:47:04 +0500

    feat: add JWT authentication guard to flags and user controllers

--- src/flags/flags.controller.ts
+++ src/flags/flags.controller.ts
@@ -7,25 +7,29 @@ import {
   Body,
   Param,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { FlagsService } from './flags.service';
 import { CreateFlagDto, UpdateFlagDto } from './dto/flags.dto';
 import { UserDto } from 'src/user/dto/user.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('flags')
 export class FlagsController {
   constructor(private readonly flagsService: FlagsService) {}
-
-  @Get()
-  getAll() {
-    return this.flagsService.getAllFlags();
-  }
 
   @Get('evaluate')
   evaluate(@Query('userId') userId: string) {
     return this.flagsService.evaluateFlagsForUser(userId);
   }
 
+  @UseGuards(AuthGuard('jwt'))
+  @Get()
+  getAll() {
+    return this.flagsService.getAllFlags();
+  }
+
+  @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Body() dto: CreateFlagDto) {
     // todo: get actorId from auth context
@@ -33,6 +37,7 @@ export class FlagsController {
     return this.flagsService.createFlag(dto, actorId);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Put(':id')
   update(@Param('id') id: string, @Body() dto: UpdateFlagDto) {
     // todo: get actorId from auth context
@@ -40,6 +45,7 @@ export class FlagsController {
     return this.flagsService.updateFlag(+id, dto, actorId);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id')
   delete(@Param('id') id: string) {
     // todo: get actorId from auth context
@@ -47,11 +53,13 @@ export class FlagsController {
     return this.flagsService.deleteFlag(+id, actorId);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Post(':id/target')
   addTarget(@Param('id') id: string, @Body() dto: UserDto) {
     return this.flagsService.addTargetUser(+id, dto);
   }
 
+  @UseGuards(AuthGuard('jwt'))
   @Delete(':id/target/:userId')
   removeTarget(@Param('id') id: string, @Param('userId') userId: string) {
     return this.flagsService.removeTargetUser(+id, userId);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly usersService: UserService) {}
 
+  @UseGuards(AuthGuard('jwt'))
   @Get()
   getAll() {
     return this.usersService.getAllUsers();
